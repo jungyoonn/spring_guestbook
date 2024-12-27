5613d864ce6d82947bfa3e07019490c547567d2f
jungyoonn <sophia7625@naver.com>
2024-12-27 11:24:44 +0900

    등록 페이지 연결

--- src/main/java/com/eeerrorcode/guestbook/controller/GuestbookController.java
+++ src/main/java/com/eeerrorcode/guestbook/controller/GuestbookController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.eeerrorcode.guestbook.domain.dto.GuestbookDto;
 import com.eeerrorcode.guestbook.domain.dto.PageRequestDto;
 import com.eeerrorcode.guestbook.service.GuestbookService;
 
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -27,5 +30,15 @@ public class GuestbookController {
     return "/guestbook/list";
   }
   
+  @GetMapping("register")
+  public void register() {
 
+  }
+  
+  @PostMapping("register")
+  public String register(GuestbookDto dto, RedirectAttributes rttr) {
+    rttr.addFlashAttribute("msg", service.write(dto));
+    return "redirect:list";
+  }
+  
 }
