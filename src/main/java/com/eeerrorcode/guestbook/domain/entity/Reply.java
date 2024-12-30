208e8a4df2b9ada5debd173cb73e0c05aa9697fc
jungyoonn <sophia7625@naver.com>
2024-12-30 13:23:48 +0900

    댓글 엔티티

---
+++ src/main/java/com/eeerrorcode/guestbook/domain/entity/Reply.java
@@ -0,0 +1,29 @@
+package com.eeerrorcode.guestbook.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Table(name = "tbl_reply")
+public class Reply extends BaseEntity{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long rno;
+
+  private String text;
+
+  private String replyer;
+
+  @ManyToOne
+  private Board board;
+}
