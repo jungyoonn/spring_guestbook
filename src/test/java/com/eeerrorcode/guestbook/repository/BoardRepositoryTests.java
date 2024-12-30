f1ec18a0ddf4b3cab95a4598960dc51cf7bb543f
jungyoonn <sophia7625@naver.com>
2024-12-30 13:24:11 +0900

    게시글 리포지토리 테스트

---
+++ src/test/java/com/eeerrorcode/guestbook/repository/BoardRepositoryTests.java
@@ -0,0 +1,57 @@
+package com.eeerrorcode.guestbook.repository;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.eeerrorcode.guestbook.domain.entity.Board;
+import com.eeerrorcode.guestbook.domain.entity.Member;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+  @Autowired
+  private BoardRepositoy repository;
+
+  @Test
+  public void testExist() {
+    log.info(repository);
+  }
+
+  @Test
+  @Transactional
+  @Rollback(false)
+  public void testInsert() {
+    IntStream.rangeClosed(2, 100).forEach(i -> {
+      Board board = Board.builder()
+      .title("title" + i)
+      .content("content" + i)
+      .member(Member.builder().email("user" + i + "@a.com").build())
+      .build();
+      repository.save(board);
+    });
+
+    // Board board = Board.builder()
+    //   .title("title2")
+    //   .content("content2")
+    //   .member(Member.builder().email("user2@a.com").build())
+    //   .build();
+    //   repository.save(board);
+  }
+
+  @Test
+  public void testSelectList() {
+    repository.findAll().forEach(log::info);
+  }
+
+  @Test
+  public void testSelectOne() {
+    log.info(repository.findById(10L));
+  }
+}
