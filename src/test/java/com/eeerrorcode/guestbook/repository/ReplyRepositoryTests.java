b4b5b3e0eefd1919e84fbdc100805e17257fced7
jungyoonn <sophia7625@naver.com>
2024-12-30 13:24:32 +0900

    댓글 리포지토리 테스트

---
+++ src/test/java/com/eeerrorcode/guestbook/repository/ReplyRepositoryTests.java
@@ -0,0 +1,50 @@
+package com.eeerrorcode.guestbook.repository;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.eeerrorcode.guestbook.domain.entity.Board;
+import com.eeerrorcode.guestbook.domain.entity.Reply;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepositoryTests {
+  @Autowired
+  private ReplyRepository repository;
+
+  @Test
+  public void testExist() {
+    log.info(repository);
+  }
+
+  @Test
+  @Transactional
+  @Rollback(false)
+  public void testInsert() {
+    IntStream.rangeClosed(1, 500).forEach(i -> {
+      Reply reply = Reply.builder()
+      .text("text" + i)
+      .replyer("replyer" + i)
+      .board(Board.builder().bno(103L + (((int)Math.random() * 99) + 1)).build())
+      .build();
+      repository.save(reply);
+    });
+  }
+
+  @Test
+  public void testSelectList() {
+    repository.findAll().forEach(log::info);
+  }
+
+  @Test
+  public void testSelectOne() {
+    log.info(repository.findById(100L));
+  }
+}
