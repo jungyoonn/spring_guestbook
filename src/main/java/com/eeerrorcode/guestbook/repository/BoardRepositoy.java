23a4ae2a17ab457291ef926f96da15ea67a00efc
jungyoonn <sophia7625@naver.com>
2024-12-30 16:15:51 +0900

    쿼리 추가

--- src/main/java/com/eeerrorcode/guestbook/repository/BoardRepositoy.java
+++ src/main/java/com/eeerrorcode/guestbook/repository/BoardRepositoy.java
@@ -1,5 +1,7 @@
 package com.eeerrorcode.guestbook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,7 @@ import com.eeerrorcode.guestbook.domain.entity.Board;
 public interface BoardRepositoy extends JpaRepository<Board, Long>{
   @Query("select b, m from tbl_board b left join member m where b.bno = :bno")
   Object getBoardWithMember(@Param("bno") Long gno);
+
+  @Query("select b, r from tbl_board b left join tbl_reply r on b = r.board where bno = :bno")
+  List<Object[]> getBoardWithReply(@Param("bno") Long bno);
 }
