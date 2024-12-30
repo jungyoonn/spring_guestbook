d1694a723a103fffb77807994a47a39263f8f95e
jungyoonn <sophia7625@naver.com>
2024-12-30 17:50:12 +0900

    쿼리 추가

--- src/main/java/com/eeerrorcode/guestbook/repository/BoardRepositoy.java
+++ src/main/java/com/eeerrorcode/guestbook/repository/BoardRepositoy.java
@@ -2,6 +2,8 @@ package com.eeerrorcode.guestbook.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,19 @@ public interface BoardRepositoy extends JpaRepository<Board, Long>{
 
   @Query("select b, r from tbl_board b left join tbl_reply r on b = r.board where bno = :bno")
   List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+  @Query(value = "select count(r), b, m\r\n" + //
+        "from tbl_board b\r\n" + //
+        "left join member m \r\n" + //
+        "left join tbl_reply r on b = r.board\r\n" + //
+        "group by b"
+  , countQuery = "select count(b) from tbl_board b")
+  Page<Object[]> getBoardWithReplyCount(Pageable pageable);
+
+  @Query("select count(r), b, m\r\n" + //
+        "from tbl_board b\r\n" + //
+        "left join member m\r\n" + //
+        "left join tbl_reply r on b = r.board\r\n" + //
+        "where bno = :bno")
+  Object[] getBoardByBno(@Param("bno") Long bno);
 }
