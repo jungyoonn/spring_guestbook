2f5c0ffbe782b506d9e8854422038fb0c572b697
jungyoonn <sophia7625@naver.com>
2024-12-30 13:24:19 +0900

    회원 리포지토리 테스트

---
+++ src/test/java/com/eeerrorcode/guestbook/repository/MemberRepositoryTests.java
@@ -0,0 +1,49 @@
+package com.eeerrorcode.guestbook.repository;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.eeerrorcode.guestbook.domain.entity.Member;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class MemberRepositoryTests {
+  @Autowired
+  private MemberRepository repository;
+
+  @Test
+  public void testExist() {
+    log.info(repository);
+  }
+
+  @Test
+  @Transactional
+  @Rollback(false)
+  public void testInsert() {
+    IntStream.rangeClosed(2, 100).forEach(i -> {
+      Member member = Member.builder()
+      .email("user" + i + "@a.com")
+      .password("1234")
+      .name("쿠키" + i)
+      .build();
+      repository.save(member);
+    });
+  }
+
+  @Test
+  public void testSelectList() {
+    repository.findAll().forEach(log::info);
+  }
+
+  @Test
+  public void testSelectOne() {
+    log.info(repository.findById("user10@a.com"));
+  }
+}
