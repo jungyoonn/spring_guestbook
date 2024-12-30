455ba4f341df60f8471e1daa6a439f5276a53934
jungyoonn <sophia7625@naver.com>
2024-12-30 13:23:42 +0900

    회원 엔티티

---
+++ src/main/java/com/eeerrorcode/guestbook/domain/entity/Member.java
@@ -0,0 +1,22 @@
+package com.eeerrorcode.guestbook.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Table(name = "tbl_member")
+public class Member extends BaseEntity{
+  @Id
+  private String email;
+
+  private String password;
+
+  private String name;
+}
