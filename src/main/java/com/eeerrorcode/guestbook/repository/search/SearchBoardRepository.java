bf2c71daa421522c46fc85ac2196f91f1731785e
jungyoonn <sophia7625@naver.com>
2024-12-31 18:20:31 +0900

    검색 리포지토리

--- src/main/java/com/eeerrorcode/guestbook/repository/search/SearchBoardRepository.java
+++ src/main/java/com/eeerrorcode/guestbook/repository/search/SearchBoardRepository.java
@@ -1,7 +1,12 @@
 package com.eeerrorcode.guestbook.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.eeerrorcode.guestbook.domain.entity.Board;
 
 public interface SearchBoardRepository {
   Board search1();
+
+  Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 }
