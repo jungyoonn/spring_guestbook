8570a3bef72d30dd814c74e9f8324bbfceae24f9
jungyoonn <sophia7625@naver.com>
2024-12-31 12:17:03 +0900

    삭제 메서드 추가

--- src/main/java/com/eeerrorcode/guestbook/repository/ReplyRepository.java
+++ src/main/java/com/eeerrorcode/guestbook/repository/ReplyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eeerrorcode.guestbook.domain.entity.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long>{
-  
+  void deleteByBoardBno(Long bno);
 }
