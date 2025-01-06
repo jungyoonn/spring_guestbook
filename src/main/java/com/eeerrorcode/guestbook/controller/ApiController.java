fb80e1672c6a6dd9a1cc4c04b7c4bd731849d949
jungyoonn <sophia7625@naver.com>
2025-01-06 12:45:59 +0900

    Cors

---
+++ src/main/java/com/eeerrorcode/guestbook/controller/ApiController.java
@@ -0,0 +1,33 @@
+package com.eeerrorcode.guestbook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+// import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("api/todo")
+public class ApiController {
+  @GetMapping("list")
+  // @CrossOrigin("http://localhost:3000")
+  public List<?> todo() {
+    List<?> list = IntStream.rangeClosed(1, 3).boxed().map(i -> {
+      Map<String, Object> map = new HashMap<>();
+      map.put("id", i);
+      map.put("content", "내용");
+      map.put("writer", "작성자");
+      map.put("regDate", LocalDateTime.now());
+      return map;
+    }).toList();
+    return list;
+  }
+  
+}
