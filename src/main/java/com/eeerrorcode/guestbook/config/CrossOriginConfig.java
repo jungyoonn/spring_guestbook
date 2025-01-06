29546232d6f41840ac3bc31162e1b7faa0717dca
jungyoonn <sophia7625@naver.com>
2025-01-06 12:46:09 +0900

    Cors 전역 설정

---
+++ src/main/java/com/eeerrorcode/guestbook/config/CrossOriginConfig.java
@@ -0,0 +1,21 @@
+package com.eeerrorcode.guestbook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfig {
+  @Bean
+  public WebMvcConfigurer configurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins("http://localhost:3000", "http://localhost");
+      }
+    };
+  } 
+}
